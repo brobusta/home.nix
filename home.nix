27bcc068db9c6778fe4cc132dcd5ada2de55922a
Vu Thanh Cong <vuthanhcong.ict@gmail.com>
2024-12-27 11:54:18 +0800

    Initial commit

---
+++ home.nix
@@ -0,0 +1,225 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "congvu";
+  home.homeDirectory = "/home/congvu";
+  home.stateVersion = "24.11";
+
+  home.packages = [
+    pkgs.tig
+    pkgs.curl
+    pkgs.wget
+    pkgs.htop
+    pkgs.eza
+    pkgs.fd
+    pkgs.bat
+    pkgs.ripgrep
+    pkgs.coreutils
+    pkgs.unzip
+    pkgs.tree
+    pkgs.wl-clipboard
+    pkgs.pfetch
+    pkgs.zsh-powerlevel10k
+    pkgs.python3Full
+    pkgs.python3.pkgs.pip
+
+    # fonts
+    pkgs.nerd-fonts.jetbrains-mono
+    pkgs.nerd-fonts.iosevka
+    pkgs.noto-fonts-emoji
+  ];
+
+  fonts= {
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        monospace = [
+          "JetbrainsMono Nerd Font"
+          "Iosevka Nerd Font"
+        ];
+        sansSerif = [
+          "JetbrainsMono Nerd Font"
+          "Iosevka Nerd Font"
+        ];
+        serif = [
+          "JetbrainsMono Nerd Font"
+          "Iosevka Nerd Font"
+        ];
+        emoji = [
+          "Noto Color Emoji"
+          "Noto Emoji"
+        ];
+      };
+    };
+  };
+
+  home.file = {
+    ".config/nvim/lua".source =  builtins.fetchGit {
+        url = "git@github.com:brobusta/nvimconfig.git";
+        rev = "e7f9cddb488118f4f957f9385ecfc27e0b335653";
+    } + "/lua";
+    ".config/nvim/init.lua".text = ''
+        require("congvu.core")
+        require("congvu.lazy")
+      '';
+  };
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    TERMINAL = "kitty";
+  };
+
+  programs.home-manager.enable = true;
+  programs.ssh.enable = true;
+
+  programs.kitty = {
+    enable = true;
+    shellIntegration.enableZshIntegration = true;
+    font = {
+      name = "JetbrainsMono Nerd Font";
+      size = 12;
+    };
+    settings = {
+      include = builtins.toString (builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/folke/tokyonight.nvim/21ad5c2f1027ed674d1b2cec73ced281f1c1c3f9/extras/kitty/tokyonight_night.conf";
+        sha256 = "e92085fb339ca9be916e3bc47fa6813843c6007b155d4f9bcb9fb5af545adc5c";
+      });
+      scrollback_lines = 100000;
+      tab_bar_style = "powerline";
+      hide_window_decorations = "yes";
+    };
+  };
+
+  programs.git = {
+    package = pkgs.gitAndTools.gitFull;
+    enable = true;
+    userName = "Vu Thanh Cong";
+    userEmail = "vuthanhcong.ict@gmail.com";
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+  };
+
+  programs.vim = {
+    enable = true;
+    extraConfig = builtins.readFile ./vimrc;
+    plugins = with pkgs.vimPlugins; [
+      fzf-vim
+      nerdtree
+      vim-gruvbox8
+      vim-airline
+      vim-airline-themes
+      vim-startify
+      # editor
+      vim-surround
+      vim-commentary
+      # linter & completion & snippet
+      ale
+      asyncomplete-vim
+      asyncomplete-lsp-vim
+      vim-lsp
+      vim-lsp-settings
+      vim-lsp-ale
+      vim-vsnip
+      vim-vsnip-integ
+    ];
+  };
+
+  programs.fzf = rec {
+    enable = true;
+    enableZshIntegration = true;
+    defaultCommand = "rg --files --no-ignore --hidden --follow --glob '!.git/*'";
+    defaultOptions = [
+    	"--height 40% --layout=reverse --border --bind 'ctrl-y:execute-silent(wl-copy {})'"
+    ];
+    changeDirWidgetCommand = "fd --type d --hidden --exclude '.git'";
+    changeDirWidgetOptions = [ "--preview 'tree -C {} | head -200'" ];
+    fileWidgetCommand = defaultCommand;
+    fileWidgetOptions = [ "--preview '(bat --style=changes --wrap never --color always {} || cat {} || (eza --tree --group-directories-first {} || tree -C {})) 2> /dev/null' --preview-window right:60%" ];
+    historyWidgetOptions = [ "--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'" ];
+  };
+
+  programs.tmux = {
+    enable = true;
+    aggressiveResize = true;
+    clock24 = true;
+    escapeTime = 0;
+    historyLimit = 1000000;
+    keyMode = "vi";
+    mouse = true;
+    prefix = "C-space";
+    terminal = "tmux-256color";
+    customPaneNavigationAndResize = true;
+
+    extraConfig = ''
+      # keybindings
+      set-option -g status-position top
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+    '';
+
+    plugins = [
+       pkgs.tmuxPlugins.tmux-fzf
+       {
+         plugin = pkgs.tmuxPlugins.tokyo-night-tmux;
+         extraConfig = ''
+           set -g @tokyo-night-tmux_show_netspeed 1
+           set -g @tokyo-night-tmux_netspeed_showip 1
+           set -g @tokyo-night-tmux_netspeed_refresh 5
+         '';
+       }
+    ];
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    history = {
+      expireDuplicatesFirst = true;
+      save = 100000000;
+      size = 100000000;
+    };
+    initExtra = ''
+      [ -f $HOME/.nix-profile/etc/profile.d/nix.sh ] && source $HOME/.nix-profile/etc/profile.d/nix.sh
+      [ -f $HOME/.p10k.zsh ] && source $HOME/.p10k.zsh
+      eval "$(direnv hook zsh)"
+    '';
+    shellAliases = {
+      l = "eza -alh";
+      ll = "eza -l";
+      ls = "eza";
+      v = "vim";
+    };
+    plugins =  [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [
+        "fzf"
+        "git"
+        "history"
+        "direnv"
+      ];
+    };
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
+
+}
+
---
+++ vimrc
@@ -0,0 +1,182 @@
+"{{{ basic settings
+set nocompatible
+set encoding=utf-8
+
+filetype indent plugin on
+syntax on
+
+set mouse=a
+set hidden
+set wildmenu
+set wildmode=longest:full,full
+set wildoptions=pum
+set list
+set listchars=tab:▸\ ,trail:·
+set showcmd
+set incsearch
+set hlsearch
+set ignorecase
+set smartcase
+set backspace=indent,eol,start
+set autoindent
+set nostartofline
+set noerrorbells visualbell t_vb=
+set number
+set relativenumber
+
+set shiftwidth=4
+set softtabstop=4
+set expandtab
+set scrolloff=8
+set splitright
+set splitbelow
+
+set clipboard=unnamed
+set foldmethod=marker
+
+nnoremap <space> <nop>
+let mapleader=" "
+
+"set omnifunc=ale#completion#OmniFunc
+let g:airline#extensions#ale#enabled = 1
+let g:ale_completion_enabled = 0
+let g:ale_sign_error = '✗'
+let g:ale_sign_warning = '⚡ '
+
+let g:rustfmt_autosave = 1
+let g:ale_fix_on_save = 1
+let g:ale_floating_preview = 1
+let g:ale_floating_window_border = repeat([''], 8)
+"}}}
+
+"{{{ colorscheme
+colorscheme gruvbox8
+set background=dark
+hi Error NONE
+"}}}
+
+"{{{ vim-airline
+let g:airline_theme='bubblegum'
+let g:airline_powerline_fonts = 1
+let g:airline_skip_empty_sections = 1
+if !exists('g:airline_symbols')
+    let g:airline_symbols = {}
+endif
+" unicode symbols
+let g:airline_left_sep = '»'
+let g:airline_left_sep = '▶'
+let g:airline_right_sep = '«'
+let g:airline_right_sep = '◀'
+let g:airline_symbols.linenr = '␊'
+let g:airline_symbols.linenr = '␤'
+let g:airline_symbols.linenr = '¶'
+let g:airline_symbols.branch = '⎇'
+let g:airline_symbols.paste = 'ρ'
+let g:airline_symbols.paste = 'Þ'
+let g:airline_symbols.paste = '∥'
+let g:airline_symbols.whitespace = 'Ξ'
+" airline symbols
+let g:airline_left_sep = ''
+let g:airline_left_alt_sep = ''
+let g:airline_right_sep = ''
+let g:airline_right_alt_sep = ''
+let g:airline_symbols.branch = ''
+let g:airline_symbols.readonly = ''
+let g:airline_symbols.linenr = ''
+let g:airline#extensions#tabline#enabled = 1
+let g:airline#extensions#tabline#buffer_nr_show = 1
+"}}}
+
+"{{{ key mappings
+" Quicker window movement
+nnoremap <leader>j <C-w>j
+nnoremap <leader>k <C-w>k
+nnoremap <leader>h <C-w>h
+nnoremap <leader>l <C-w>l
+nnoremap <leader>wf :w!<cr>
+nnoremap <leader>q :bd<cr>
+nnoremap <leader>wq :wq<cr>
+" Vertical and horizontal splits
+nnoremap <leader>vs <C-w>v
+nnoremap <leader>xs <C-w>S
+nnoremap <silent> <leader>+ :vertical resize +5<cr>
+nnoremap <silent> <leader>- :vertical resize -5<cr>
+nnoremap <S-l> :bnext<cr>
+nnoremap <S-h> :bprevious<cr>
+nnoremap <leader>u :bprevious<cr>
+tnoremap <leader><esc> <C-\><C-n>
+" Keep it centered
+nnoremap n nzzzv
+nnoremap N Nzzzv
+nnoremap J mzJ`z
+vnoremap < <gv
+vnoremap > >gv
+vnoremap p "_dP
+vnoremap y myy`y
+vnoremap Y myY`y
+nnoremap Y y$
+nnoremap <leader>xo :!xdg-open %<cr><cr>
+nnoremap <C-a> <esc>ggVG
+imap jk <esc>
+imap ;; <esc>A;<esc>
+nmap <silent> gf :edit <cfile><cr>
+nnoremap <leader>nt :NERDTreeToggle %<cr>
+nnoremap <leader>f :Files<cr>
+nnoremap <leader>b :Buffers<cr>
+nnoremap <leader>l :BLines<cr>
+nnoremap <leader>rg :Rg<cr>
+nnoremap <leader>gb :GBranches<cr>
+nmap <F2> :Maps<cr>
+cnoremap w!! w !sudo tee > /dev/null %
+
+" vim-lsp
+function! s:on_lsp_buffer_enabled() abort
+  setlocal omnifunc=lsp#complete
+  setlocal signcolumn=yes
+  if exists('+tagfunc') | setlocal tagfunc=lsp#tagfunc | endif
+  nmap <buffer> gd <plug>(lsp-definition)
+  nmap <buffer> gs <plug>(lsp-document-symbol-search)
+  nmap <buffer> gS <plug>(lsp-workspace-symbol-search)
+  nmap <buffer> gr <plug>(lsp-references)
+  nmap <buffer> gi <plug>(lsp-implementation)
+  nmap <buffer> <leader>gt <plug>(lsp-type-definition)
+  nmap <buffer> <leader>rn <plug>(lsp-rename)
+  nmap <buffer> <leader>ca <plug>(lsp-code-action)
+  nmap <buffer> [g <plug>(ale_previous_wrap)
+  nmap <buffer> ]g <plug>(ale_next_wrap)
+  nmap <buffer> K <plug>(lsp-hover)
+  nnoremap <buffer> <expr><c-d> lsp#scroll(+4)
+  nnoremap <buffer> <expr><c-u> lsp#scroll(-4)
+
+  let g:lsp_format_sync_timeout = 1000
+  autocmd! BufWritePre *.rs,*.go call execute('LspDocumentFormatSync')
+endfunction
+
+augroup lsp_install
+    au!
+    " call s:on_lsp_buffer_enabled only for languages that has the server registered.
+    autocmd User lsp_buffer_enabled call s:on_lsp_buffer_enabled()
+augroup END
+
+" asyncomplete
+inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
+inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
+inoremap <expr> <cr>    pumvisible() ? asyncomplete#close_popup() : "\<cr>"
+imap <c-@> <plug>(asyncomplete_force_refresh)
+
+" snippet
+imap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
+smap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
+imap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
+smap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
+imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+
+" editor
+nnoremap <leader>wf :w!<cr>
+nnoremap <leader>Q :qa!<cr>
+vnoremap <C-r> "hy:%s,<C-r>h,,g<left><left>
+
+"}}}
